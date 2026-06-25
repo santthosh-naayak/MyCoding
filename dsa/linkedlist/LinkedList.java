dd682475115547d882a0d1b57e716c1cea7e3157
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2026-06-25 23:18:35 +0530

    LinkedList methods to print

--- dsa/linkedlist/LinkedList.java
+++ dsa/linkedlist/LinkedList.java
@@ -1,23 +1,52 @@
 package dsa.linkedlist;
 
 public class LinkedList {
+    // Class variables
     private Node head;
     private Node tail;
     private int length;
 
+    // Nested class Node
     class Node {
         int value;
         Node next;
 
+        // Node Constructor
         Node(int value) {
             this.value = value;
         }
     }
 
+    // LinkedList Constructor
     public LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
+
+    // Method to print the linked list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    // Method to print Head
+    public void getHead() {
+        System.out.println("Head : " + head.value);
+    }
+
+    // Method to print Tail
+    public void getTail() {
+        System.out.println("Tail : " + tail.value);
+    }
+
+    // Method to print Length of LinkedList
+    public void getLength() {
+        System.out.println("Length : " + length);
+    }
+
 }
--- dsa/linkedlist/Main.java
+++ dsa/linkedlist/Main.java
@@ -3,6 +3,10 @@ package dsa.linkedlist;
 public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
-        System.out.println(myLinkedList);
+        System.out.println(myLinkedList); // Prints address
+        myLinkedList.getHead();// Prints the Head of linkedList
+        myLinkedList.getTail();// Prints the Tail of linkedList
+        myLinkedList.getLength();// Prints the length of linkedList
+        myLinkedList.printList();// Prints the linkedList
     }
 }
