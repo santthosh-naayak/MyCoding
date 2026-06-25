886cced6e85824a80b9afca81f2cbdfb7276d890
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2026-06-25 22:47:16 +0530

    Linked List Constructor Node Exercise

---
+++ dsa/linkedlist/LinkedList.java
@@ -0,0 +1,23 @@
+package dsa.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+}
